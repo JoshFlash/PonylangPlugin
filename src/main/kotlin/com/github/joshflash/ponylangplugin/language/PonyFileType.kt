f5a96c5a739aef6172ee4bc43d898720a5dd120d
Flash <joshuaflash@gmail.com>
2024-11-27 23:58:06 +1300

    Add pony file extension recognition

---
+++ src/main/kotlin/com/github/joshflash/ponylangplugin/language/PonyFileType.kt
@@ -0,0 +1,16 @@
+package com.github.joshflash.ponylangplugin.language
+
+import com.intellij.openapi.fileTypes.LanguageFileType
+import javax.swing.Icon
+
+class PonyFileType : LanguageFileType(PonyLanguage) {
+    companion object {
+        @JvmStatic
+        val INSTANCE = PonyFileType()
+    }
+
+    override fun getName(): String = "Pony"
+    override fun getDescription(): String = "Pony"
+    override fun getDefaultExtension(): String = "pony"
+    override fun getIcon(): Icon? = PonyIcons.FILE
+}
---
+++ src/main/kotlin/com/github/joshflash/ponylangplugin/language/PonyIcons.kt
@@ -0,0 +1,8 @@
+package com.github.joshflash.ponylangplugin.language
+
+import com.intellij.openapi.util.IconLoader
+
+object PonyIcons {
+    @JvmField
+    val FILE = IconLoader.getIcon("/icons/pony-icon.svg", PonyIcons::class.java)
+}
